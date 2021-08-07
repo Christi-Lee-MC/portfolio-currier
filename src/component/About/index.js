510c5ae8246f650cae32d0f9e27e02a74e62a709
Christi-Lee <christileemc@gmail.com>
2021-08-07 09:04:15 -0700

    added material ui to test <typography> for style and moved img above intro line

--- src/component/About/index.js
+++ src/component/About/index.js
@@ -5,17 +5,20 @@ import Typography from '@material-ui/core/Typography';
 function About() {
     return (
         <section className="my-5" >
-          <h1 id="about">Christi-Lee Currier</h1>
-          {<img src={headerImage} className="my-2" style={{ width: "100%" }} alt="header" />}
+           {<img src={headerImage} className="my-2" style={{ width: "100%" }} alt="header" />}
+           
+          <h1 id="about">A little bit about me...</h1>
+         
           <div className="my-2">
-            <p>
+            <Typography>
               Hello!  I am Christi-Lee Currier, most people call me Christi. 
-              </p><p>My background is in marketing, management, small business ownership, 
+              </Typography>
+              <Typography>My background is in marketing, management, small business ownership, 
                 baking bread, a Weird Al fan and the Mom to 2 human kids and 4 feline kids. 
 
                 blah blah blah I have no idea what to write here but will add more after the 
                 site is functional.
-          </p>
+          </Typography>
          
           
 
